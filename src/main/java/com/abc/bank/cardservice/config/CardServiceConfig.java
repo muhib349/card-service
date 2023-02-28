873c1fe99ed21129af40e70cdbeda868d00e5cd2
Mohib <gazi.mohib@brainstation-23.com>
2023-02-28 21:22:34 +0000

    setup config client for card service

--- src/main/java/com/abc/bank/cardservice/config/CardServiceConfig.java
+++ src/main/java/com/abc/bank/cardservice/config/CardServiceConfig.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 @Configuration
-@ConfigurationProperties(prefix = "loans")
+@ConfigurationProperties(prefix = "cards")
 @Getter
 @Setter
 public class CardServiceConfig {
--- src/main/java/com/abc/bank/cardservice/controller/CardsController.java
+++ src/main/java/com/abc/bank/cardservice/controller/CardsController.java
@@ -1,9 +1,15 @@
 package com.abc.bank.cardservice.controller;
 
+import com.abc.bank.cardservice.config.CardServiceConfig;
 import com.abc.bank.cardservice.model.Cards;
+import com.abc.bank.cardservice.model.ConfigProperties;
 import com.abc.bank.cardservice.model.Customer;
 import com.abc.bank.cardservice.repository.CardsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +22,22 @@ public class CardsController {
     @Autowired
     private CardsRepository cardsRepository;
 
+    @Autowired
+    private CardServiceConfig serviceConfig;
     @PostMapping("/myCards")
     public List<Cards> getCardDetails(@RequestBody Customer customer) {
         return cardsRepository.findByCustomerId(customer.getCustomerId());
     }
 
+    @GetMapping("/cards/properties")
+    public String getConfigProperties() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        ConfigProperties properties = new ConfigProperties(
+                serviceConfig.getMsg(),
+                serviceConfig.getBuildVersion(),
+                serviceConfig.getMailDetails(),
+                serviceConfig.getActiveBranches()
+        );
+        return ow.writeValueAsString(properties);
+    }
 }
